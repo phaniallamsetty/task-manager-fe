344e11c92d1430b136dd956fa5dc4489dc01b77c
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2024-11-29 18:09:51 -0500

    Removed router reference to fix failing tests. Will add later.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/shared/header/header.component";
 import { FooterComponent } from "./components/shared/footer/footer.component";
 import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
